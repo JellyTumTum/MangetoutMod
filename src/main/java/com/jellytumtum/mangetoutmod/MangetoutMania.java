35ff92655a6fed7a9b547d030016de87a1a5bf43
JellyTumTum <cameronst12345@gmail.com>
2021-05-30 23:41:13 +0100

    Recipes and zombie loot table
    Added All recipes for current items

--- src/main/java/com/jellytumtum/mangetoutmod/MangetoutMania.java
+++ src/main/java/com/jellytumtum/mangetoutmod/MangetoutMania.java
@@ -2,11 +2,35 @@ package com.jellytumtum.mangetoutmod;
 
 import com.jellytumtum.mangetoutmod.registry.ModBlocks;
 import com.jellytumtum.mangetoutmod.registry.ModItems;
+
+import jdk.nashorn.api.tree.IdentifierTree;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.util.Identifier;
+
+/* IMPORTANT DETAILS :
+
+- TO ADD AN ITEM you need to add to ModItems and ModItems.registerItems + in lang folder add entry + add texture.
+- TO ADD BLOCK you need to add to ModBlocks + ModItems + ModItems.registerItems + ModBlocks.registerBlocks + in lang folder add entry and texture.
+
+TO DO LIST: 
+
+- Farmable Mangetout
+- Food Functionality to Mangetout. 
+- Mangetout weapons
+- Mangetoutium Ore.
+- Magic Mangetout
+- Mangetouter - drops mangetout (creeper model)
+
+*/
 
 public class MangetoutMania implements ModInitializer {
 
@@ -26,11 +50,34 @@ public class MangetoutMania implements ModInitializer {
         })
         .build();
 
+    
+        // injecting additional entries into loot tables. 
+        private static final Identifier ZOMBIE_LOOT_TABLE_ID = new Identifier("minecraft", "entities/zombie");
+
     @Override
     public void onInitialize() {
         ModItems.registerItems();
         ModBlocks.registerBlocks();
         
+        modifyLootTables();
+        
+    }
+
+    private void modifyLootTables() {
+        LootTableLoadingCallback.EVENT.register(((rescourceManager, lootManager, id, supplier, setter) -> {
+            // checks for the specific loot table
+            if (ZOMBIE_LOOT_TABLE_ID.equals(id)) {
+
+                // Add individual drop
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(ModItems.MANGETOUT)) // Add individual drop
+                        .withFunction(SetCountLootFunction.builder(UniformLootTableRange.between(0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+
+            // Extra Loot Tables go inside another if statement innit.
+            }
+        }));
     }
     
 }
--- src/main/java/com/jellytumtum/mangetoutmod/registry/ModItems.java
+++ src/main/java/com/jellytumtum/mangetoutmod/registry/ModItems.java
@@ -3,6 +3,7 @@ package com.jellytumtum.mangetoutmod.registry;
 import com.jellytumtum.mangetoutmod.MangetoutMania;
 
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -11,7 +12,16 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     // Items
-    public static final Item MANGETOUT = new Item(new Item.Settings().group(ItemGroup.FOOD));
+    public static final Item MANGETOUT = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder()
+        .hunger(4)
+        .saturationModifier(2f)
+        .snack()
+        .build()));  // DOES NOT NEED TO BE INDENTED JUST CLEAR THAT ITS PART OF FOOD COMPONENT BUILDER
+    public static final Item CRISP_MANGETOUT = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder()
+        .hunger(2)
+        .snack()
+        .build())); 
+
 
     // Block Items
     public static final BlockItem CONDENSED_MANGETOUT = new BlockItem(ModBlocks.CONDENSED_MANGETOUT, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
@@ -19,5 +29,6 @@ public class ModItems {
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(MangetoutMania.MOD_ID, "mangetout"), MANGETOUT);
         Registry.register(Registry.ITEM, new Identifier(MangetoutMania.MOD_ID, "condensed_mangetout"), CONDENSED_MANGETOUT);
+        Registry.register(Registry.ITEM, new Identifier(MangetoutMania.MOD_ID, "crisp_mangetout"), CRISP_MANGETOUT);
     }
 }
